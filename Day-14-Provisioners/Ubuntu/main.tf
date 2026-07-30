ea298d16017d0cc6d917aee1c61f4c1392cfbffe
mozibur-786 <khanrohit7311@gmail.com>
2026-07-30 19:38:50 +0530

    added variables

--- Day-14-Provisioners/Ubuntu/main.tf
+++ Day-14-Provisioners/Ubuntu/main.tf
@@ -84,8 +84,8 @@ resource "aws_security_group" "my_sg" {
 
 # ec2 instance (ubuntu)
 resource "aws_instance" "my_ec2" {
-    ami                         = "ami-0b6d9d3d33ba97d99"   # ubuntu ami
-    instance_type               = "t2.micro"
+    ami                         = var.ami_id   # ubuntu ami
+    instance_type               = var.instance_type
     key_name = aws_key_pair.my_key.key_name
     subnet_id                   = aws_subnet.my_subnet.id
     vpc_security_group_ids      = [aws_security_group.my_sg.id]
---
+++ Day-14-Provisioners/Ubuntu/terraform.tfvars
@@ -0,0 +1,2 @@
+ami_id = "ami-0b6d9d3d33ba97d99"
+instance_type = "t2.micro"
