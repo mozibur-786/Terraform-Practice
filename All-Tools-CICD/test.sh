0bbf029332a24bb9f14e102d040e237107084639
Mozibur Rahman Khan <khanrohit7311@gmail.com>
2026-08-07 17:03:09 +0530

    Update Java installation to version 21

--- All-Tools-CICD/test.sh
+++ All-Tools-CICD/test.sh
@@ -8,7 +8,7 @@ sudo yum install git -y
 
 #-------java dependency for jenkins------------
 
-sudo yum install java-17-amazon-corretto.x86_64
+sudo yum install java-21-amazon-corretto -y
 
 
 #------------jenkins install-------------
